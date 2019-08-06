004c26b7ab3bd8e503976421d1444a21d98efb5e
Salvatore Allegra <salallegra@gmail.com>
2019-08-06 00:51:51 -0300

    Add home controller

--- src/main/java/com/cooksys/orm/OrmApplication.java
+++ src/main/java/com/cooksys/orm/OrmApplication.java
@@ -2,10 +2,12 @@ package com.cooksys.orm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+
 @EnableSwagger2
 public class OrmApplication {
 
---
+++ src/main/java/com/cooksys/orm/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.cooksys.orm.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/")
+public class HomeController {
+	
+	@GetMapping
+	public String Home() {
+		return "<h1>Social Media Api";
+	}
+
+}
